5b76a85d1d5caa195660d12d71d5081d9ac9d395
Haya Alhuraib <hayawi8002@gmail.com>
2024-12-09 21:12:57 +0300

    Update naive.c

--- src/mmult_Optimized/impl/naive.c
+++ src/mmult_Optimized/impl/naive.c
@@ -8,9 +8,6 @@
 /* Include application-specific headers */
 #include "include/types.h"
 
-/* Naive Implementation */
-#pragma GCC push_options
-#pragma GCC optimize ("O1")
 
 void* impl_scalar_naive(void* args) {
     args_t* a = (args_t*)args;
@@ -32,4 +29,3 @@ void* impl_scalar_naive(void* args) {
     }
     return NULL;
 }
-#pragma GCC pop_options
